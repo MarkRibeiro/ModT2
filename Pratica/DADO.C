60a91ff9cef4820eb9f1dfed610c2c9e961605a9
unknown <markpribeiro@hotmail.com>
2019-05-13 22:00:39 -0300

    resolve commit errado

--- Pratica/DADO.C
+++ Pratica/DADO.C
@@ -1,19 +1,47 @@
+/***************************************************************************
+*
+*  $MCD Módulo de definição: Módulo dado
+*
+*  Arquivo gerado:              DADO.C
+*  Letras identificadoras:      DAD
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: Disciplina INF 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: mr - Mark Ribeiro
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data    Observações
+*       3.00   mr   11/05/2019 Documentação
+*		2.00   mr   05/05/2019 Final do desenvolvimento
+*       1.00   mr   27/04/2019 Início do desenvolvimento
+*
+***************************************************************************/
+
 #include "DADO.H"
 
-int* jogaDados(int *d)
-{
-	time_t t;
-	srand((unsigned) time(&t));
-
-	d[0] = (rand() % 6) + 1;
-	d[1] = (rand() % 6) + 1;
-
-	if(d[0] == d[1])
+/***************************************************************************
+*
+*  Função: DAD Joga dados
+*  ****/
+	DAD_tpCondRet DAD_JogaDados(int *d)
 	{
-		d[2] = d[0];
-		d[3] = d[0];
-	}
+		time_t t;
+		srand((unsigned) time(&t));
 
-	return d;
-}
+		d[0] = (rand() % 6) + 1;
+		d[1] = (rand() % 6) + 1;
+
+		if(d[0] == d[1])
+		{
+			d[2] = d[0];
+			d[3] = d[0];
+		} /* if */
+
+		return DAD_CondRetOK;
+	}  /* Fim função: DAD Joga dados */
+
+/********** Fim do módulo de implementação: Módulo dado **********/
 
--- Pratica/peca.c
+++ Pratica/peca.c
@@ -1,23 +1,97 @@
+/***************************************************************************
+*
+*  $MCD Módulo de definição: Módulo peça
+*
+*  Arquivo gerado:              PECA.H
+*  Letras identificadoras:      PEC
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: Disciplina INF 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: mr - Mark RIbeiro
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data    Observações
+*       3.00   mr   11/05/2019 Documentaçâo
+*       2.00   mr   10/05/2019 Final do desenvolvimento
+*       1.00   mr   01/05/2019 Início do desenvolvimento
+*
+***************************************************************************/
+
 #include "PECA.H"
 
-struct peca{
+/***********************************************************************
+*
+*  $TC Tipo de dados: PEC Descritor do nó da árvore
+*
+*
+*  $ED Descrição do tipo
+*     Descreve a organização do nó
+*
+***********************************************************************/
+
+typedef struct peca{
 	char cor;
-};
+}Peca;
 
-Peca* criaPeca( char cor )
-{
-	Peca *nova = (Peca*) malloc (sizeof(Peca));
-	nova->cor = cor;
+/***************************************************************************
+*
+*  Função: PEC Cria peça
+*  ****/
 
-	return nova;
-}
+	PEC_tpCondRet PEC_CriaPeca( Peca **p,  char cor )
+	{
+		*p = (Peca*) malloc (sizeof(Peca));
 
-void leCorPeca ( Peca *p )
-{
-	printf("Cor da Peça: %c\n", p->cor);
-}
+		if(*p==NULL)
+		{
+			return PEC_CondRetFaltouMemoria;
+		} /* if */
 
-void destroiPeca( Peca *p )
-{
-	free(p);
-}
+		else
+		{
+			(*p)->cor = cor;
+
+			return PEC_CondRetOK;
+		} /* if */
+	}  /* Fim função: PEC Cria peça */
+
+/***************************************************************************
+*
+*  Função: PEC Obter peça
+*  ****/
+	PEC_tpCondRet PEC_ObterCorPeca ( Peca *p, char *cor )
+	{
+		if( p==NULL )
+		{
+			return PEC_CondRetPecaNaoExiste;
+		} /* if */
+
+		*cor=p->cor;
+		if(*cor!='b' && *cor!='p')
+		{
+			return PEC_CondRetInvalida;
+		} /* if */
+
+		return PEC_CondRetOK; 
+	} /* Fim função: PEC Obter peça */
+
+/***************************************************************************
+*
+*  Função: PEC Destroi peça
+*  ****/
+	PEC_tpCondRet PEC_DestroiPeca( Peca *p )
+	{
+		if(p==NULL)
+		{
+			return PEC_CondRetPecaNaoExiste;
+		} /* if */
+	 
+		free(p);
+
+		return PEC_CondRetOK;
+	} /* Fim função: PEC Destroi peça */
+
+/********** Fim do módulo de implementação: Módulo peça **********/
